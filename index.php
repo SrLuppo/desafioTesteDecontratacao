98646948f048bfbd6ad2b76909cd2629b14f8a28
SrLuppo <92128439+SrLuppo@users.noreply.github.com>
2021-12-18 09:53:45 -0300

    criação dos arquivos
    primeira paginas do teste

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Teste de para vaga de programador junior</title>
+</head>
+<body>
+    <header>
+        <div>
+
+        </div>
+    </header>
+    <div class="noticia1">
+
+    </div>
+    <div class="noticia2">
+        
+    </div>
+    <div class="noticia3">
+        
+    </div>
+    <div class="noticia4">
+        
+    </div>
+    <div class="noticia5">
+        
+    </div>
+    <div class="noticia6">
+        
+    </div>
+    
+</body>
+</html>
