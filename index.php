39072aa532f75b05017d9b1ec1087e4c37da6050
SrLuppo <92128439+SrLuppo@users.noreply.github.com>
2021-12-18 12:45:19 -0300

    criação do layout
    coloquei alguns botões e parágrafos

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 ?>
 
 <!DOCTYPE html>
@@ -14,28 +15,60 @@
 </head>
 <body>
     <header>
-        <div>
+        <a href="#">logo</a>
+        <nav>
+            <ul class="menu">
+                <li><a href="#">Cadastrar Noticia</a></li>
+                <li><a href="#">Exibir Noticias</a></li>
+                <li><input type="text" name="" id=""></a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>Noticias</h1>
+    <section class="noticia">
+        <div class="noticia" id="1">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, doloremque ratione? Ullam, optio consequatur ducimus veniam voluptas magni praesentium quos ratione officiis dolor hic, voluptatum distinctio iure architecto asperiores excepturi?
+            </p>
+            <input type="button" value="Acessar">
 
         </div>
-    </header>
-    <div class="noticia1">
-
-    </div>
-    <div class="noticia2">
-        
-    </div>
-    <div class="noticia3">
-        
-    </div>
-    <div class="noticia4">
-        
-    </div>
-    <div class="noticia5">
-        
-    </div>
-    <div class="noticia6">
-        
-    </div>
+        <div class="noticia"id="2">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti consectetur error voluptas earum tempora ipsam iure perspiciatis aliquam impedit laboriosam maxime rem cumque quos nostrum culpa officia assumenda, reprehenderit a.
+            </p>
+            <input type="button" value="Acessar">
+        </div>
+        <div class="noticia"id="3">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit deleniti incidunt molestiae quod accusantium! Magnam eos, quis porro qui impedit suscipit et ipsam cupiditate tenetur rerum sed voluptate saepe.
+            </p>
+            <input type="button" value="Acessar">
+        </div>
+        <div class="noticia"id="4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sit quidem labore, facere dolores soluta ratione minus qui quod quasi molestias exercitationem, voluptatum nemo, laudantium officia aperiam itaque enim animi!
+            </p>
+            <input type="button" value="Acessar">
+        </div>
+        <div class="noticia"id="5">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed in corrupti ratione unde, et illum obcaecati fugiat consectetur assumenda voluptate aliquid explicabo cupiditate eum dignissimos dicta, expedita fuga, nam autem.adfasdfsdfsadfsadfsadf
+            </p>
+            <input type="button" value="Acessar">
+            
+        </div>
+        <div class="noticia"id="6">
+            
+            <p>
+                <strong>Tilulo da Noticia</strong><br>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, ut libero consectetur provident sapiente, mollitia nobis debitis repellendus labore obcaecati ducimus aspernatur error fugiat hic, quod officia placeat quia eligendi.
+            </p>
+            <input type="button" value="Acessar">
+            
+        </div>
+    </section>
     
 </body>
+<footer> Projeto Desenvolvido pelo Adelano</footer>
 </html>
